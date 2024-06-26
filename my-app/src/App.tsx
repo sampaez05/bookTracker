877df9381f634cd162f91e5b08c823ebfcc22e98
sptherose <sptherose@gmail.com>
2024-06-26 17:07:45 -0400

    made bookshelves into a bookcase and cutified the add-book button and the "welcome to interactive  booktracker" title

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { isBindingElement } from 'typescript';
+
+function add_book(){
+
+}
 
 function App() {
+
   return (
     <div className="App">
+      <div className="header">
+        <span className="title">Welcome to Interactive BookTracker</span>
+        <br></br>
+        <button className="add-book">Add Book</button>
+      </div>
+     {/* <img src="bookcase.png" alt="Bookcase"></img> */}
+      <div className="bookshelf1">
+        <span className="books"></span>
+      </div>
+      <div className="bookshelf2">
+        <span className="books"></span>
+      </div>
+      <div className="bookshelf3">
+        <span className="books"></span>
+      </div>
+      <div className="bookshelf4">
+        <span className="books"></span>
+      </div>
     </div>
   );
 }
+
 
 export default App;
