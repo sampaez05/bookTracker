3a5d15c160c60a9ebda8c06996821fbd72ea7d04
sptherose <sptherose@gmail.com>
2024-08-15 16:13:14 -0400

    commented/documented the book class in book.tsx

--- my-app/src/book.tsx
+++ my-app/src/book.tsx
@@ -15,6 +15,15 @@ export class Book {
       this.translator = translator;
       this.rating = rating;
       this.review = review;
+    /**
+     * This dataclass represents a book
+     * Attributes:
+     * title:string - the name of the book
+     * author:string - the person who wrote the book
+     * translator:string - the person who translated the book, if applicable
+     * rating:string - the number of stars the user would rate the book
+     * review:string - the user's review/thoughts/opinions about the book
+     */
     }
   }
 
